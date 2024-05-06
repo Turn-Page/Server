8ae74fa5384fe4217dd4bfc26e0ce827464f2111
tnals2384 <tnals2384@gmail.com>
2024-05-06 22:21:57 +0900

    feat: #13 friend Entity 생성

---
+++ src/main/java/com/example/turnpage/domain/friend/entity/Friend.java
@@ -0,0 +1,29 @@
+package com.example.turnpage.domain.friend.entity;
+
+import com.example.turnpage.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@SQLDelete(sql = "UPDATE friend SET deleted_at = CURRENT_TIMESTAMP WHERE friend_id = ?")
+@SQLRestriction("deleted_at is NULL")
+public class Friend {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "friend_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id", nullable = false)
+    private Member sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private Member receiver;
+}
