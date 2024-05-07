a92a06ab12857f930b4dd0f559f4ed268171c3f1
tnals2384 <tnals2384@gmail.com>
2024-05-07 15:07:11 +0900

    fix: #12 member image 컬럼 text로 변경

--- src/main/java/com/example/turnpage/global/config/SecurityConfig.java
+++ src/main/java/com/example/turnpage/global/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -24,6 +25,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
+    private final AuthenticationConfiguration authenticationConfiguration;
     private final JwtUtils jwtUtils;
     private final MemberRepository memberRepository;
     private final CustomOAuth2UserService customOAuth2UserService;
