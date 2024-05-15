30c37bb15ea8d7432c3b132d0e309f2e8ce7efd4
tnals2384 <tnals2384@gmail.com>
2024-05-15 17:15:21 +0900

    feat: #24 BookScheduler 생성, saveBestSeller 스케줄러 생성

---
+++ src/main/java/com/example/turnpage/domain/book/scheduler/BookScheduler.java
@@ -0,0 +1,27 @@
+package com.example.turnpage.domain.book.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class BookScheduler {
+    private final BookService bookService;
+    @Scheduled(cron = "0 0 4 * * MON") //초 분 시 일 월 요일
+    public void saveBestSeller() {
+        try {
+            bookService.saveBestSeller();
+            log.info("베스트셀러 저장이 완료되었습니다.");
+
+        } catch (InterruptedException e) {
+            log.info("Thread가 강제 종료되었습니다. Message: {}", e.getMessage());
+        } catch (Exception e) {
+            log.info("Batch 시스템이 예기치 않게 종료되었습니다. Message: {}", e.getMessage());
+        }
+
+
+    }
+}
