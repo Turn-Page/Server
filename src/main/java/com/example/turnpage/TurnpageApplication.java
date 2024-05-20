eeffb4566b7208308e7d44f9b897770c97424fc2
tnals2384 <tnals2384@gmail.com>
2024-05-20 23:27:29 +0900

    chore: open api base url 변경

--- src/main/java/com/example/turnpage/TurnpageApplication.java
+++ src/main/java/com/example/turnpage/TurnpageApplication.java
@@ -3,7 +3,6 @@ package com.example.turnpage;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -12,7 +11,7 @@ import java.util.TimeZone;
 
 @EnableJpaAuditing
 @EnableScheduling
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class TurnpageApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TurnpageApplication.class, args);
