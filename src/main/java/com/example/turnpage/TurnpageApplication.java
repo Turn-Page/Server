4b8fafd5b995ea9017f71ff423247f7c390a0e90
tnals2384 <tnals2384@gmail.com>
2024-05-20 22:41:51 +0900

    chore: TimeZone 변경 확인

--- src/main/java/com/example/turnpage/TurnpageApplication.java
+++ src/main/java/com/example/turnpage/TurnpageApplication.java
@@ -7,20 +7,20 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.time.LocalDate;
+import java.time.ZonedDateTime;
 import java.util.TimeZone;
 
 @EnableJpaAuditing
 @EnableScheduling
-@SpringBootApplication(/*exclude = SecurityAutoConfiguration.class*/)
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class TurnpageApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TurnpageApplication.class, args);
 	}
 
 	@PostConstruct
-	public void setTimeZone(){
+	public void setTimeZone() {
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-		System.out.println("타임존 설정 후 현재 시각: " + LocalDate.now());
+		System.out.println("타임존 설정 후 현재 시각: " + ZonedDateTime.now());
 	}
 }
