34156e225a77c64d88418aea21df4989d9392204
tnals2384 <tnals2384@gmail.com>
2024-05-26 00:31:04 +0900

    test: #31 판매글 저장 성공 서비스 테스트 코드 작성

---
+++ src/test/java/com/example/turnpage/domain/salePost/service/SalePostServiceTest.java
@@ -0,0 +1,54 @@
+package com.example.turnpage.domain.salePost.service;
+
+import com.example.turnpage.domain.book.dto.BookRequest.SaveBookRequest;
+import com.example.turnpage.domain.salePost.entity.Grade;
+import com.example.turnpage.domain.salePost.entity.SalePost;
+import com.example.turnpage.support.ServiceTestConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@DisplayName("Sale Post 서비스의")
+public class SalePostServiceTest extends ServiceTestConfig {
+
+    @Autowired
+    SalePostService salePostService;
+    @Test
+    @DisplayName("판매글 저장 성공 테스트")
+    public void saveSalePost() {
+        //given
+        SaveBookRequest bookRequest = SaveBookRequest.builder()
+                .itemId(1L)
+                .title("꿈꾸지 않아도 빤짝이는 중 - 놀면서 일하는 두 남자 삐까뚱씨, 내일의 목표보단 오늘의 행복에 집중하는 인생로그")
+                .author("브로디, 노아")
+                .cover("https://image.aladin.co.kr/product/33948/74/coversum/k392930236_1.jpg")
+                .isbn("12987349382")
+                .publisher("포레스트북스")
+                .publicationDate("2023-12-18")
+                .description("삐까뚱씨라는 이름으로 유튜브를 하고 있는 브로디와 노아.")
+                .build();
+
+        SaveSalePostRequest request = SaveSalePostRequest.builder()
+                 .title("제목")
+                 .description("설명")
+                 .grade(Grade.MINT)
+                 .price(10000)
+                 .bookInfo(bookRequest)
+                 .build();
+        //when
+        SalePost salePost = salePostService.findSalePost(salePostService.saveSalePost(member, request).getSalePostId());
+
+        //then
+        assertEquals(salePost.getDescription(), "제목");
+        assertEquals(salePost.getDescription(), "설명");
+        assertEquals(salePost.getPrice(), 10000);
+        assertEquals(salePost.getGrade(), Grade.MINT);
+        assertEquals(salePost.getBook().getItemId(),1);
+        assertEquals(salePost.getMember().getName(),"수밈");
+    }
+
+}
